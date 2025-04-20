9472ae047b8c4c9180b76242af465b034ab01671
HasutoSasaki <hasuto.sa@gmail.com>
2025-04-20 10:36:55 +0900

    feat: add expo-camera and expo-cli dependencies.

---
+++ components/view/ViewCamera.tsx
@@ -0,0 +1,70 @@
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export function ViewCamera() {
+    const [facing, setFacing] = useState<CameraType>('back');
+    const [camera, setCamera] = useState(null);
+    const [permission, requestPermission] = useCameraPermissions();
+
+    if (!permission) return <View />;
+
+    if (!permission.granted) {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.message}>We need your permission to show the camera</Text>
+                <Button onPress={requestPermission} title="grant permission" />
+            </View>
+        );
+    }
+
+    async function toggleCameraFacing() {
+        if (camera) {
+            const photo = await camera.takePictureAsync()
+            console.log(photo);
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <CameraView style={styles.camera} facing={facing}
+                ref={(ref) => setCamera(ref)}>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+                        <Text style={styles.text}>take picture</Text>
+                    </TouchableOpacity>
+                </View>
+            </CameraView>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    message: {
+        textAlign: 'center',
+        paddingBottom: 10,
+    },
+    camera: {
+        flex: 1,
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: 'transparent',
+        margin: 64,
+    },
+    button: {
+        flex: 1,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+});
