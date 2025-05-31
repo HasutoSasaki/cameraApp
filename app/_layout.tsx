f7c5b1879c8a88a20f0310fd0fb864a6c68bd038
HasutoSasaki <hasuto.sa@gmail.com>
2025-05-31 18:08:41 +0900

    feat: Wrap in View component to make the background black

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
 import { Stack } from 'expo-router/stack';
+import { View } from 'react-native';
 
 export default function Layout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" />
-    </Stack>
+    <View style={{ flex: 1, backgroundColor: 'black' }}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+    </View>
+
   );
 }
