20491fd391ee8a7dd65278eb0d79ade342ff7b0a
HasutoSasaki <hasuto.sa@gmail.com>
2025-06-10 07:36:01 +0900

    feat: add image assets for camera application

---
+++ assets/index.ts
@@ -0,0 +1,7 @@
+
+export const images = {
+    soloIcon: require('@/assets/images/icon/solo.png'),
+    soloStandIcon: require('@/assets/images/icon/solo_stand.png'),
+    groupIcon: require('@/assets/images/icon/group.png'),
+    selfieIcon: require('@/assets/images/icon/selfie.png'),
+};
