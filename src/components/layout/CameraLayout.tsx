fc71d98e0d45ec1743d5a14c2c0dd21e5a51209d
HasutoSasaki <hasuto.sa@gmail.com>
2025-07-10 06:56:17 +0900

    feat: integrate RenderCamera functionality into CameraLayout

--- src/components/layout/CameraLayout.tsx
+++ src/components/layout/CameraLayout.tsx
@@ -1,14 +1,20 @@
+import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import { CameraView, useCameraPermissions } from 'expo-camera';
-import { Image } from "expo-image";
-import { useState, useRef } from 'react';
-import { Button, StyleSheet, Pressable, Text, View, Modal } from 'react-native';
 import * as MediaLibrary from 'expo-media-library';
-import { RenderCamera } from '../camera/RenderCamera';
+import { useCamera } from '@/hooks/useCamera';
 import { RenderRequestPermission } from '../camera/RenderRequestPermission';
+import { RenderCameraGesture } from '../camera/RenderCameraGesture';
+import { RenderTiltIndicator } from '../camera/RenderTiltIndicator';
+import { TopControlBar } from '../ui/controls/TopControlBar';
+import { BottomControlBar } from '../ui/controls/BottomControlBar';
 
 export function CameraLayout() {
     const [permission, requestPermission] = useCameraPermissions();
     const [mediaPermission, requestMediaPermission] = MediaLibrary.usePermissions();
+    const { ref, zoom, handleCameraReady, aspectRatio, handleZoomChange, handleRatioChange, ratio, takePicture, lastPhoto, photos } = useCamera();
+    // top control bar state
+    const [isLevelIndicatorVisible, setIsLevelIndicatorVisible] = useState(true);
 
     if (!permission || !mediaPermission) return <View />;
 
@@ -21,7 +27,29 @@ export function CameraLayout() {
 
     return (
         <View style={styles.container}>
-            <RenderCamera />
+            <TopControlBar ratio={ratio} setRatio={handleRatioChange}
+                isLevelIndicatorVisible={isLevelIndicatorVisible} setIsLevelIndicatorVisible={setIsLevelIndicatorVisible} />
+            <CameraView
+                style={[styles.camera, { aspectRatio }]}
+                ref={ref}
+                mute={false}
+                zoom={zoom}
+                responsiveOrientationWhenOrientationLocked
+                onCameraReady={handleCameraReady}
+                ratio={ratio}
+            />
+            <RenderCameraGesture
+                handleZoomChange={handleZoomChange}
+                zoom={zoom}
+            />
+            <RenderTiltIndicator isVisible={isLevelIndicatorVisible} />
+            <BottomControlBar
+                photos={photos}
+                zoom={zoom}
+                handleZoomChange={handleZoomChange}
+                takePicture={takePicture}
+                lastPhoto={lastPhoto}
+            />
         </View>
     );
 }
@@ -33,5 +61,10 @@ const styles = StyleSheet.create({
         backgroundColor: "#000",
         alignItems: "center",
         justifyContent: "center",
+    },
+
+    camera: {
+        width: "100%",
+        aspectRatio: 4 / 3,
     },
 });
